826fccd8ebcb69405c2db67eb0362661269e99a2
rncrmn <aaronleadgeex@gmail.com>
2023-03-26 22:50:21 +0800

    add try catch on fetch

--- src/hooks/useKWExtractor.tsx
+++ src/hooks/useKWExtractor.tsx
@@ -8,28 +8,32 @@ export default function useKWExtractor(): KWExtractor {
   const extractKeywords = async (initialText: string) => {
     const [text, setText] = createSignal(initialText);
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt:
-          "Extract the important keywords from this text. Make the first letter of each word uppercase and separate them with commas\n\n" +
-          text(),
-        temperature: 0.5,
-        max_tokens: 60,
-        frequency_penalty: 0.8,
-      }),
-    };
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "text-davinci-003",
+          prompt:
+            "Extract the important keywords from this text. Make the first letter of each word uppercase and separate them with commas\n\n" +
+            text(),
+          temperature: 0.5,
+          max_tokens: 60,
+          frequency_penalty: 0.8,
+        }),
+      };
 
-    const response = await fetch(import.meta.env.VITE_OPENAI_URL, options);
-    const json = await response.json();
-    const data = json.choices[0].text;
+      const response = await fetch(import.meta.env.VITE_OPENAI_URL, options);
+      const json = await response.json();
+      const data = await json.choices[0].text;
 
-    return data;
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return { extractKeywords };
